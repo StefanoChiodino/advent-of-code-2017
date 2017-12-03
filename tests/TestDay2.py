ed95b6130e7d757b2d349bb04867ec4bf99066fa
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-12-03 13:12:22 +0000

    day2

---
+++ tests/TestDay2.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.day2 import day2
+
+
+class TestDay2(unittest.TestCase):
+
+    def test_example_1(self):
+        input = [[5, 1, 9, 5],
+                 [7, 5, 3],
+                 [2, 4, 6, 8]]
+        self.assertEqual(day2(input), 18)
