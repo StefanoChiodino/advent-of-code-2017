4f647e72895f86806e68eeaafe5d5a6bf294865c
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-12-04 19:12:30 +0000

    day2b

--- tests/TestDay2.py
+++ tests/TestDay2.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.day2 import day2
+from src.day2 import day2a, day2b
 
 
 class TestDay2(unittest.TestCase):
@@ -9,4 +9,10 @@ class TestDay2(unittest.TestCase):
         input = [[5, 1, 9, 5],
                  [7, 5, 3],
                  [2, 4, 6, 8]]
-        self.assertEqual(18, day2(input))
+        self.assertEqual(18, day2a(input))
+
+    def test_example_2(self):
+        input = [[5, 9, 2, 8],
+                 [9, 4, 7, 3],
+                 [3, 8, 6, 5]]
+        self.assertEqual(9, day2b(input))
