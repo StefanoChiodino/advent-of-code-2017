e2c5a1b2c9a53ae34623d439bd12a031eca740f9
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-12-09 21:56:29 +0000

    day67

---
+++ src/day7.py
@@ -0,0 +1,30 @@
+import re
+
+import os
+
+
+def day7(instructions):
+    discs = []
+    for match in [re.match(r"(\w*)\s?\((\d*)\)?\s?-?>?\s?((\w*,?\s?)*)", instruction) for instruction in instructions]:
+        disc = Disc()
+        disc.name = match.group(1)
+        disc.weight = match.group(2)
+        disc.children = match.group(3).split(", ") if match.group(3) is not None else None
+        discs.append(disc)
+
+    all_discs_names = [d.name for d in discs]
+    for disc in [d for d in discs if d.children is not None]:
+        for children_disc in disc.children:
+            if children_disc in all_discs_names:
+                all_discs_names.remove(children_disc)
+    return all_discs_names[0]
+
+
+class Disc:
+    pass
+
+
+if __name__ == "__main__":
+    path = os.path.join("..", "inputs", "day7_inputs.txt")
+    lines = [l.strip() for l in open(path)]
+    print(day7(lines))
